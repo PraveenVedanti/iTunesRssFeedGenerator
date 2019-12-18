a7f38e6e1780e27e0a548b863f87868bfb7cfe6c
Praveen <praveen@Praveens-MacBook-Pro-5.local>
2019-12-18 15:16:03 -0800

    first commit

---
+++ iTunesRssFeedGeneratorTests/iTunesRssFeedGeneratorTests.swift
@@ -0,0 +1,34 @@
+//
+//  iTunesRssFeedGeneratorTests.swift
+//  iTunesRssFeedGeneratorTests
+//
+//  Created by Praveen on 12/18/19.
+//  Copyright © 2019 Praveen. All rights reserved.
+//
+
+import XCTest
+@testable import iTunesRssFeedGenerator
+
+class iTunesRssFeedGeneratorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
